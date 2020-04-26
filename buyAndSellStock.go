375f3bc03dad82b67da56b0d58e0b8de3f24f5d3
Warlin Felipe Reyes <warlin16@gmail.com>
2020-04-26 18:01:03 -0400

    solve best day to buy and sell stock

---
+++ buyAndSellStock.go
@@ -0,0 +1,34 @@
+package main
+
+/**
+Say you have an array for which the ith element
+is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction
+(i.e., buy one and sell one share of the stock),
+design an algorithm to find the maximum profit.
+
+Note that you cannot sell a stock before you buy one.
+
+Input: [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+             Not 7-1 = 6, as selling price needs to be larger than buying price.
+
+*/
+
+func maxProfit(prices []int) int {
+	lowestPrice := prices[0]
+	var profit int
+	for _, price := range prices {
+		if lowestPrice > price {
+			lowestPrice = price
+		} else {
+			currentProfit := price - lowestPrice
+			if currentProfit > profit {
+				profit = currentProfit
+			}
+		}
+	}
+	return profit
+}
--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("The result", twoSum([]int{2, 7, 11, 15}, 9))
+
 }
