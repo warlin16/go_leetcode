82942ed8446d0d908b0d35cdf42501783e36ceb0
Warlin Felipe Reyes <warlin16@gmail.com>
2020-04-27 19:59:34 -0400

    solve 217 contains duplicate

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(":eyes:", containsDuplicate([]int{1, 2, 3, 1}))
+	fmt.Println(":eyes:", containsDuplicate([]int{1, 2, 3, 4}))
+	fmt.Println(":eyes:", containsDuplicate([]int{1, 1, 1, 3, 3, 4, 3, 1, 4, 2}))
 }
