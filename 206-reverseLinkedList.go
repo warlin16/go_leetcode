6299d99c2e85c9cd1facfc45828d8c8287d1ae88
Warlin Felipe Reyes <warlin16@gmail.com>
2020-04-27 19:45:22 -0400

    reset main.go

--- 206-reverseLinkedList.go
+++ 206-reverseLinkedList.go
@@ -47,3 +47,30 @@ func reverseListOptimized(head *ListNode) *ListNode {
 	}
 	return newHead
 }
+
+/**
+var head *ListNode = &ListNode{
+	Val:  1,
+	Next: node2,
+}
+
+var node2 *ListNode = &ListNode{
+	Val:  2,
+	Next: node3,
+}
+
+var node3 *ListNode = &ListNode{
+	Val:  3,
+	Next: node4,
+}
+
+var node4 *ListNode = &ListNode{
+	Val:  4,
+	Next: node5,
+}
+
+var node5 *ListNode = &ListNode{
+	Val:  5,
+	Next: nil,
+}
+*/
--- main.go
+++ main.go
@@ -1,36 +1,5 @@
 package main
 
-import "fmt"
-
-var head *ListNode = &ListNode{
-	Val:  1,
-	Next: node2,
-}
-
-var node2 *ListNode = &ListNode{
-	Val:  2,
-	Next: node3,
-}
-
-var node3 *ListNode = &ListNode{
-	Val:  3,
-	Next: node4,
-}
-
-var node4 *ListNode = &ListNode{
-	Val:  4,
-	Next: node5,
-}
-
-var node5 *ListNode = &ListNode{
-	Val:  5,
-	Next: nil,
-}
-
 func main() {
-	test := reverseList(head)
-	for current := test; current != nil; {
-		fmt.Println("This should work", current.Val)
-		current = current.Next
-	}
+
 }
