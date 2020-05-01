fb888e6a58ebcabccbfacefff96c158f41ae4b05
Warlin Felipe Reyes <warlin16@gmail.com>
2020-04-30 20:37:22 -0400

    add optimized solution for problem 3

--- 3-longestSubstringWithoutRepeatingChar.go
+++ 3-longestSubstringWithoutRepeatingChar.go
@@ -22,24 +22,41 @@ Explanation: The answer is "wke", with the length of 3.
 Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 */
 
+// func lengthOfLongestSubstring(s string) int {
+// 	m := make(map[string]int)
+// 	increment := 0
+// 	longest := 0
+// 	for i := 0; i < len(s); i++ {
+// 		letter := string(s[i])
+// 		if m[letter] != 0 {
+// 			i = increment
+// 			m = make(map[string]int)
+// 			increment++
+// 			continue
+// 		}
+// 		m[letter]++
+// 		if len(m) > longest {
+// 			longest = len(m)
+// 		}
+// 	}
+// 	return longest
+// }
+
+// brute force
+
 func lengthOfLongestSubstring(s string) int {
-	m := make(map[string]int)
-	increment := 0
-	longest := 0
-	for i := 0; i < len(s); i++ {
-		letter := string(s[i])
-		if m[letter] != 0 {
-			i = increment
-			m = make(map[string]int)
-			increment++
-			continue
+	var current, longest int
+	var m = make(map[rune]int)
+	for i, char := range s {
+		if start1, found := m[char]; found {
+			if start1+1 > current {
+				current = start1 + 1
+			}
 		}
-		m[letter]++
-		if len(m) > longest {
-			longest = len(m)
+		if longest < i-current+1 {
+			longest++
 		}
+		m[char] = i
 	}
 	return longest
 }
-
-// brute force
