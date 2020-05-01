c301dcdd3c517f69d574f8ff7a932e1d0c7519ab
Warlin Felipe Reyes <warlin16@gmail.com>
2020-05-01 10:57:06 -0400

    submit 136-single number problem for may leetcode challenge

---
+++ 136-singleNumber.go
@@ -0,0 +1,41 @@
+package main
+
+/**
+Given a non-empty array of integers,
+every element appears twice except for one.Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity.
+Could you implement it without using extra memory?
+
+Example 1:
+
+Input: [2,2,1]
+Output: 1
+Example 2:
+
+Input: [4,1,2,1,2]
+Output: 4
+*/
+
+// func singleNumber(nums []int) int {
+// 	m := map[int]int{}
+// 	for i := range nums {
+// 		m[nums[i]]++
+// 	}
+// 	for k, v := range m {
+// 		if v == 1 {
+// 			return k
+// 		}
+// 	}
+// 	return 0
+// }
+
+func singleNumber(nums []int) int {
+	r := 0
+	for _, n := range nums {
+		r ^= n
+	}
+	return r
+}
--- main.go
+++ main.go
@@ -1,4 +1,8 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println(singleNumber([]int{2, 2, 1}))
+	fmt.Println(singleNumber([]int{4, 1, 2, 1, 2}))
 }
