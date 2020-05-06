7cf316be31bc715fb19689e89b7e8aaab45aa33d
Warlin Felipe Reyes <warlin16@gmail.com>
2020-05-06 13:28:20 -0400

    solve the leetcode problem of the day

---
+++ easy/169-majorityElement.go
@@ -0,0 +1,20 @@
+package easy
+
+// array of size n -> n / 2
+// [3, 2, 3] -> n / 2 == 1.5 so majorityElement is 3 since it appears
+// more than n / 2
+// [2, 2, 1, 1, 1, 2, 2] -> 2
+
+func majorityElement(nums []int) int {
+	numToCheck := len(nums) / 2
+	m := make(map[int]int)
+	for _, n := range nums {
+		m[n]++
+	}
+	for k, v := range m {
+		if v > numToCheck {
+			return k
+		}
+	}
+	return 0
+}
