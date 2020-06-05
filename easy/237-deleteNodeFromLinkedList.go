7496548a8826db45c1ec9dace5a40c5f64758724
Warlin Felipe Reyes <warlin16@gmail.com>
2020-06-04 23:16:25 -0400

    optimize solution

--- easy/237-deleteNodeFromLinkedList.go
+++ easy/237-deleteNodeFromLinkedList.go
@@ -1,14 +1,5 @@
 package easy
 
 func deleteNode(node *ListNode) {
-	for node.Next != nil {
-		if node.Next.Next == nil {
-			break
-		}
-		node.Val = node.Next.Val
-		node = node.Next
-	}
-	node.Val = node.Next.Val
-	node.Next = nil
-
+	node.Val, node.Next = node.Next.Val, node.Next.Next
 }
